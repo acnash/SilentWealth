5267ab941be56cd9047358e4d65375c8d9c87c1d
acnash <anthony.nash@dpag.ox.ac.uk>
2024-12-29 11:51:06 +0000

    Initial commit

--- silent_wealth_solo.py
+++ silent_wealth_solo.py
@@ -31,8 +31,8 @@ global_ema20 = None
 global_first_adjustment = True
 global_previous_ema = None
 
-#number_shares = 0
 
+# number_shares = 0
 
 
 def sell_market_order(ib_input, symbol, quantity_input, stock_input):
@@ -42,11 +42,11 @@ def sell_market_order(ib_input, symbol, quantity_input, stock_input):
     if not holding_stock:
         return
     else:
-        #print("...Selling")
-        #positions = ib_input.positions()
-        #position = next((p for p in positions if p.contract.symbol == symbol), None)
+        # print("...Selling")
+        # positions = ib_input.positions()
+        # position = next((p for p in positions if p.contract.symbol == symbol), None)
 
-        #quantity_to_sell = position.position
+        # quantity_to_sell = position.position
         print(f"...Selling {quantity_input} of {symbol}")
 
         # Create a market order to sell all shares
@@ -93,13 +93,55 @@ def sell_market_order(ib_input, symbol, quantity_input, stock_input):
         # else:
         #     print("Market price not available.")
 
+
 def place_market_BTC_order(ib_input, stock_input, quantity_input, cash):
-    pass
+    # This is for Bitcoin
+    # order = MarketOrder('BUY', 0)
+    global holding_stock
+    global global_buy_price
+    global global_ema20
+    global global_first_adjustment
+    global global_previous_ema
 
-def sell_market_BTC_order(ib_input, stock_input):
-    pass
+    if not holding_stock:
+        share_ticker = ib_input.reqMktData(stock_input, '', False, False)
+        ib_input.sleep(2)
+        bid_price = share_ticker.bid
+        print(f"...Placing limit order of {quantity_input} of BTC at {bid_price} per share")
+        order = LimitOrder('BUY', quantity_input, bid_price)
+        order.cashQty = cash
+        order.tif = 'IOC'
+        trade = ib_input.placeOrder(stock_input, order)
+        holding_stock = True
 
-def place_market_order(ib_input, stock_input, quantity_input, symbol, stop_loss_percent, cash):
+        while not trade.isDone():
+            ib_input.waitOnUpdate()
+
+        if trade.fills:
+            fill_price = trade.fills[0].execution.price
+            print(f"Trade filled at {fill_price} price per share")
+            stop_loss_offset = 1.000 - stop_loss_percent
+            stop_loss_price = round(fill_price * stop_loss_offset, 3)
+            print(f"Stop-loss price set at {stop_loss_price:.3f}")
+            stop_order = StopOrder('SELL', quantity_input, stop_loss_price)
+    else:
+        if global_ema20 > global_buy_price and global_first_adjustment:
+            print(
+                f"EMA20 {global_ema20} > original buy price. Adjusting stop loss to {global_ema20} to track the curve and avoid losses.")
+            stop_order = StopOrder('SELL', quantity_input, global_ema20)
+            trade = ib_input.placeOrder(stock_input, stop_order)
+            global_previous_ema = global_ema20
+            global_first_adjustment = False
+        elif not global_first_adjustment and global_ema20 > global_previous_ema:
+            print(
+                f"EMA20 {global_ema20} > previous EMA20 {global_previous_ema}. Adjusting stop loss to {global_ema20} to track the curve and avoid losses.")
+            stop_order = StopOrder('SELL', quantity_input, global_ema20)
+            trade = ib_input.placeOrder(stock_input, stop_order)
+            global_previous_ema = global_ema20
+
+
+
+def place_market_order(ib_input, stock_input, quantity_input, symbol, stop_loss_percent):
     # buy here...
     global holding_stock
     global global_buy_price
@@ -108,75 +150,48 @@ def place_market_order(ib_input, stock_input, quantity_input, symbol, stop_loss_
     global global_previous_ema
 
     if holding_stock:
-        if not symbol == "BTC":
-            #ticker = ib_input.reqMktData(stock_input, '', False, False)
-            #current_price = ticker.last
-            if global_ema20 > global_buy_price and global_first_adjustment:
-                print(f"EMA20 {global_ema20} > original buy price. Adjusting stop loss to {global_ema20} to track the curve and avoid losses.")
-                stop_order = StopOrder('SELL', quantity_input, global_ema20)
-                trade = ib_input.placeOrder(stock_input, stop_order)
-                global_previous_ema = global_ema20
-                global_first_adjustment = False
-            elif not global_first_adjustment and global_ema20 > global_previous_ema:
-                print(f"EMA20 {global_ema20} > previous EMA20 {global_previous_ema}. Adjusting stop loss to {global_ema20} to track the curve and avoid losses.")
-                stop_order = StopOrder('SELL', quantity_input, global_ema20)
-                trade = ib_input.placeOrder(stock_input, stop_order)
-                global_previous_ema = global_ema20
-        else:
-            # For bitcoin - to finish
-            pass
-        return
+        # ticker = ib_input.reqMktData(stock_input, '', False, False)
+        # current_price = ticker.last
+        if global_ema20 > global_buy_price and global_first_adjustment:
+            print(
+                f"EMA20 {global_ema20} > original buy price. Adjusting stop loss to {global_ema20} to track the curve and avoid losses.")
+            stop_order = StopOrder('SELL', quantity_input, global_ema20)
+            trade = ib_input.placeOrder(stock_input, stop_order)
+            global_previous_ema = global_ema20
+            global_first_adjustment = False
+        elif not global_first_adjustment and global_ema20 > global_previous_ema:
+            print(
+                f"EMA20 {global_ema20} > previous EMA20 {global_previous_ema}. Adjusting stop loss to {global_ema20} to track the curve and avoid losses.")
+            stop_order = StopOrder('SELL', quantity_input, global_ema20)
+            trade = ib_input.placeOrder(stock_input, stop_order)
+            global_previous_ema = global_ema20
     else:
-        if not symbol == "BTC":
-            #btc_ticker = ib_input.reqMktData(stock_input, '', False, False)
-            #ib_input.sleep(2)
-            #bid_price = btc_ticker.bid
-            #print(f"...Placing limit order of {quantity_input} of {symbol} shares at {bid_price} per share")
-            #buy_order = LimitOrder('BUY', quantity_input, bid_price)
-            buy_order = MarketOrder('BUY', quantity_input)  # 'BUY' indicates the action and 10 is the quantity of shares
-            print(f"...Placing market order of {quantity_input} of {symbol} shares.")
-            trade = ib_input.placeOrder(stock_input, buy_order)
+        # btc_ticker = ib_input.reqMktData(stock_input, '', False, False)
+        # ib_input.sleep(2)
+        # bid_price = btc_ticker.bid
+        # print(f"...Placing limit order of {quantity_input} of {symbol} shares at {bid_price} per share")
+        # buy_order = LimitOrder('BUY', quantity_input, bid_price)
+        buy_order = MarketOrder('BUY', quantity_input)  # 'BUY' indicates the action and 10 is the quantity of shares
+        print(f"...Placing market order of {quantity_input} of {symbol} shares.")
+        trade = ib_input.placeOrder(stock_input, buy_order)
 
-            # Wait for the order to fill
-            while not trade.isDone():
-                ib_input.waitOnUpdate()
+        # Wait for the order to fill
+        while not trade.isDone():
+            ib_input.waitOnUpdate()
 
-            if trade.fills:
-                holding_stock = True
-                fill_price = trade.fills[0].execution.price
-                global_buy_price = fill_price
-                print(f"Trade filled at {fill_price} price per share")
-                stop_loss_offset = 1.000 - stop_loss_percent
-                stop_loss_price = round(fill_price * stop_loss_offset, 3)
-                print(f"Stop-loss price set at {stop_loss_price:.3f}")
-                stop_order = StopOrder('SELL', quantity_input, stop_loss_price)
-                trade = ib_input.placeOrder(stock_input, stop_order)
-                print(trade)
-            else:
-                print("ERROR: trade not filled. Please check the Trader Workstation.")
-        else:
-            # This is for Bitcoin
-            #order = MarketOrder('BUY', 0)
-            share_ticker = ib_input.reqMktData(stock_input, '', False, False)
-            ib_input.sleep(2)
-            bid_price = share_ticker.bid
-            print(f"...Placing limit order of {quantity_input} of BTC at {bid_price} per share")
-            order = LimitOrder('BUY', quantity_input, bid_price)
-            order.cashQty = cash
-            order.tif = 'IOC'
-            trade = ib_input.placeOrder(stock_input, order)
+        if trade.fills:
             holding_stock = True
-
-            while not trade.isDone():
-                ib_input.waitOnUpdate()
-
-            if trade.fills:
-                fill_price = trade.fills[0].execution.price
-                print(f"Trade filled at {fill_price} price per share")
-                stop_loss_offset = 1.000 - stop_loss_percent
-                stop_loss_price = round(fill_price * stop_loss_offset, 3)
-                print(f"Stop-loss price set at {stop_loss_price:.3f}")
-                stop_order = StopOrder('SELL', quantity_input, stop_loss_price)
+            fill_price = trade.fills[0].execution.price
+            global_buy_price = fill_price
+            print(f"Trade filled at {fill_price} price per share")
+            stop_loss_offset = 1.000 - stop_loss_percent
+            stop_loss_price = round(fill_price * stop_loss_offset, 3)
+            print(f"Stop-loss price set at {stop_loss_price:.3f}")
+            stop_order = StopOrder('SELL', quantity_input, stop_loss_price)
+            trade = ib_input.placeOrder(stock_input, stop_order)
+            print(trade)
+        else:
+            print("ERROR: trade not filled. Please check the Trader Workstation.")
 
 
 def silent_wealth_start(ib_input, stock_input, frame_size, stock_name):
@@ -220,7 +235,8 @@ def decision_maker(ib_input, stock_input, frame_size, stock_name):
 
 
 # Define a wrapper function to execute only within the desired hours
-def scheduled_task(ib_input, stock_input, ticker_name_input, quantity_input, frame_size, stop_loss_percent, dollar_amount, trade_time):
+def scheduled_task(ib_input, stock_input, ticker_name_input, quantity_input, frame_size, stop_loss_percent,
+                   dollar_amount, trade_time):
     global holding_stock
     current_time = datetime.now().time()
 
@@ -232,8 +248,7 @@ def scheduled_task(ib_input, stock_input, ticker_name_input, quantity_input, fra
         elif action == SELL:
             sell_market_order(ib_input, ticker_name_input, quantity_input, stock_input)
         elif action == BUY:
-            place_market_order(ib_input, stock_input, quantity_input, ticker_name_input, stop_loss_percent,
-                               dollar_amount)
+            place_market_BTC_order(ib_input, stock_input, quantity_input, dollar_amount)
     else:
         start_process = False
         if trade_time == "US":
@@ -253,7 +268,7 @@ def scheduled_task(ib_input, stock_input, ticker_name_input, quantity_input, fra
             elif action == SELL:
                 sell_market_order(ib_input, ticker_name_input, quantity_input, stock_input)
             elif action == BUY:
-                place_market_order(ib_input, stock_input, quantity_input, ticker_name_input, stop_loss_percent, dollar_amount)
+                place_market_order(ib_input, stock_input, quantity_input, ticker_name_input, stop_loss_percent)
         else:
             # outside of trading hours for regular stock
             close_down_trades = False
@@ -283,13 +298,16 @@ def scheduled_task(ib_input, stock_input, ticker_name_input, quantity_input, fra
 parser = argparse.ArgumentParser(description="Silent Wealth")
 
 # Add arguments
-parser.add_argument("--ticker_name", type=str, help="Ticker name e.g., BP. SOXL SOXS, and BTC for Bitcoin", required=True)
-parser.add_argument("--exchange", type=str, help="Exchange e.g., LSE LSEETF ARCA. Not required for Bitcoin.", required=False)
+parser.add_argument("--ticker_name", type=str, help="Ticker name e.g., BP. SOXL SOXS, and BTC for Bitcoin",
+                    required=True)
+parser.add_argument("--exchange", type=str, help="Exchange e.g., LSE LSEETF ARCA. Not required for Bitcoin.",
+                    required=False)
 parser.add_argument("--currency", type=str, help="Currency for trade e.g., USD, GBP.", required=True)
 parser.add_argument("--quantity", type=int, help="The number of shares to buy/sell", required=False)
 parser.add_argument("--frame_size", type=int, help="Minute candle size e.g., 1, 5, or 10", required=True)
 parser.add_argument("--account", type=str, help="Account type e.g., paper or live", required=True)
-parser.add_argument("--stop_loss_percent", type=float, help="The percent below the buy position to take as a stop-loss", required=True)
+parser.add_argument("--stop_loss_percent", type=float, help="The percent below the buy position to take as a stop-loss",
+                    required=True)
 parser.add_argument("--dollar_amount", type=int, help="Amount of bitcoin to buy in dollars", required=False)
 parser.add_argument('--time', choices=['US', 'UK'], required=True, help='Specify the region (US or UK).')
 args = parser.parse_args()
@@ -344,7 +362,6 @@ else:
         print("Cannot determine market price. Possible connection issue.")
         ib.disconnect()
         exit()
-
 
 print("---------Silent Wealth------------")
 print(f"Ticker nane: {ticker_name}")
